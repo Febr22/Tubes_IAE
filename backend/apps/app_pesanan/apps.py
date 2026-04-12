539b5032f4b72e4d05ea2da424181baef68d2248
Febr22 <febrianalif22@gmail.com>
2026-04-12 20:11:56 +0700

    Fix: Backend folder now tracked correctly

---
+++ backend/apps/app_pesanan/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppPesananConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.app_pesanan'  # <-- Tambahkan 'apps.' di bagian ini
---
+++ backend/apps/app_pesanan/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Menunggu Pembayaran'),
+        ('diproses', 'Sedang Diproses'),
+        ('dikirim', 'Dalam Pengiriman'),
+        ('selesai', 'Selesai'),
+        ('dibatalkan', 'Dibatalkan'),
+    )
+
+    pembeli_id = models.IntegerField() # Referensi ke ID User di db_utama
+    total_harga = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    catatan = models.TextField(blank=True, null=True)
+    tanggal_pesan = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'tabel_order'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    laptop_id = models.IntegerField() # Referensi ke ID Laptop di db_ms_katalog
+    jumlah = models.IntegerField(default=1)
+    harga_saat_beli = models.DecimalField(max_digits=12, decimal_places=2)
+
+    class Meta:
+        db_table = 'tabel_order_item'
