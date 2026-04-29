9a8cb99ab30a7a662f90a5164ad47b3d3d8f14e1
cathyptr <cathyputeri05@gmail.com>
2026-04-29 22:51:08 +0700

    Menambahkan fitur pemesanan produk

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("API Backend Jalan 🚀")),
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.app_users.urls')),
     path('api/katalog/', include('apps.app_katalog.urls')),
-    
-    # Beri tanda pagar (#) di depan baris ini untuk sementara
-    # path('api/katalog/', include('apps.app_katalog.urls')), 
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
